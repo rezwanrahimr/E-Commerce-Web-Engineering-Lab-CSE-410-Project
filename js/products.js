38e2c7d1de681e764729e99f1eab711184721809
rezwanrahimr <rezwanrahim69@gmail.com>
2024-03-23 16:41:33 +0600

    added products & footer

--- js/products.js
+++ js/products.js
@@ -1,29 +1,9 @@
-const nav = document.querySelector(".nav"),
-    searchIcon = document.querySelector("#searchIcon"),
-    navOpenBtn = document.querySelector(".navOpenBtn"),
-    navCloseBtn = document.querySelector(".navCloseBtn");
-searchIcon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    nav.classList.remove("openNav");
-    if (nav.classList.contains("openSearch")) {
-        return searchIcon.classList.replace("uil-search", "uil-times");
-    }
-    searchIcon.classList.replace("uil-times", "uil-search");
-});
-navOpenBtn.addEventListener("click", () => {
-    nav.classList.add("openNav");
-    nav.classList.remove("openSearch");
-    searchIcon.classList.replace("uil-times", "uil-search");
-});
-navCloseBtn.addEventListener("click", () => {
-    nav.classList.remove("openNav");
-});
 
 // Load Products Data
 fetch('products.json')
     .then(response => response.json())
     .then(data => {
-        renderProducts(data.slice(0, 15));
+        renderProducts(data);
     })
     .catch(error => {
         alert('Error Product Fetching... ', error)
